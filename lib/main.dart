1abf16d19d1797c052b2e34ea972714c8cf85353
Stefano <stef.pro@hotmail.com>
2022-09-18 19:25:01 -0400

    added input bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,47 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primaryColor: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('To-Do'),
+          title: Text('To-Do List'),
           centerTitle: true,
         ),
+        body: InputField(),
       ),
+    );
+  }
+}
+
+class InputField extends StatefulWidget {
+  const InputField({super.key});
+
+  @override
+  State<InputField> createState() => _InputFieldState();
+}
+
+class _InputFieldState extends State<InputField> {
+  TextEditingController textController = TextEditingController();
+  String displayText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextFormField(
+          textAlign: TextAlign.center,
+          controller: textController,
+          maxLines: null,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Enter a to do',
+          ),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                displayText = textController.text;
+              });
+            },
+            child: Text("Add To-Do")),
+      ],
     );
   }
 }
